169d7427eb7cd5d8aa853c49d0354d23625c5908
Tasos Psychogyiopoulos <tasospsy@gmail.com>
2024-06-02 10:03:34 +0300

    ch3 improve Tasos figs & png

--- r-figures-code/ch3-r-figures-DRAFT.R
+++ r-figures-code/ch3-r-figures-DRAFT.R
@@ -73,12 +73,17 @@ if(length(e1$cluster) == nrow(x)){
 x %>% 
   ggplot(aes(x = week, y = dep, color = cluster))+
   geom_line(color = 'black', linewidth = .4) +
-  geom_point(aes( fill = cluster), size = 1.5, shape = 21, stroke = .5, color = 'black')+
+  geom_point(aes( fill = cluster), size = 3, shape = 21, stroke = .5, color = 'black')+
   scale_fill_gradient(low = 'white', high = colors[4])+
   labs(x = 'Week', y = 'SLC-90')+
   theme_minimal()+
-  theme1 + theme(legend.position = 'none')
-ggsave('media/ch3/fig-ch3-img19-old-31.png', width = 3, height = 1.5, units = 'in', dpi = 300)
+  theme1 + theme(
+    legend.position = 'none',
+    axis.title.x = element_text(size = 60),
+    axis.title.y = element_text(size = 60),
+    axis.text.x  = element_text(size = 60),
+    axis.text.y  = element_text(size = 60))
+ggsave('media/ch3/fig-ch3-img19-old-31.png', width = 8, height = 4, units = 'in', dpi = 300)
 
 
 # fig 3.20 ----------------------------------------------------------------
@@ -138,8 +143,8 @@ plotdat <- bind_cols(b = dat[,1], linear = dat[,4], cusp = dat[,5])
 p <- plotdat %>% 
   pivot_longer(2:3,names_to = 'mod', values_to = 'bic') %>% 
   ggplot(aes(x = b, y = bic))+
-  geom_line(aes(color = mod, linetype = mod), linewidth = .3)+
-  geom_point(aes(color = mod, shape = mod), size = 1.5)+
+  geom_line(aes(color = mod, linetype = mod), linewidth = .6)+
+  geom_point(aes(color = mod, shape = mod), size = 3)+
   geom_vline(xintercept = 0, linetype = 'dotted')+
   ylim(600, 1400) +
   scale_x_continuous(n.breaks = 6) +
@@ -149,13 +154,20 @@ p <- plotdat %>%
         color = '', 
         shape = '', linetype = '')+
   theme_minimal()+
-  theme1 + theme(legend.position = 'right')
+  theme1 + theme1 + theme(
+    legend.position = 'right',
+    axis.title.x = element_text(size = 60),
+    axis.title.y = element_text(size = 60),
+    axis.text.x  = element_text(size = 60),
+    axis.text.y  = element_text(size = 60),
+    legend.text  = element_text(size = 40),
+    legend.title = element_text(size = 40))
 p + annotate("text", x=-.5, y=800, label="no hysteresis",
-            color="grey20", size = 7) +
+            color="grey20", size = 20) +
   annotate("text", x=.5, y=800, label="hysteresis",
-           color="grey20", size = 7) 
+           color="grey20", size = 20) 
 
-ggsave('media/ch3/fig-ch3-img23-old-35.jpg', width = 3.5, height = 2, units = 'in', dpi = 300)
+ggsave('media/ch3/fig-ch3-img23-old-35.png', width = 8, height = 5, units = 'in', dpi = 300)
 
 # fig 3.24 ----------------------------------------------------------------
 
